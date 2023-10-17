db2bc9a7b799f5d44d40fed4d3050fa276d8cdc7
Awena Salic <asalic@student.42.fr>
2023-10-17 16:27:02 +0200

    NORME: export check

--- src/Builtins_Awe/utils_built2.c
+++ src/Builtins_Awe/utils_built2.c
@@ -6,7 +6,7 @@
 /*   By: asalic <asalic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 09:31:12 by asalic            #+#    #+#             */
-/*   Updated: 2023/10/17 11:52:07 by asalic           ###   ########.fr       */
+/*   Updated: 2023/10/17 16:26:11 by asalic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,3 +68,58 @@ void	add_env(t_lexer **env_list, char *str)
 //     }
 //     return (new_head);
 // }
+
+/*
+ * Gere export sans args
+ * Affiche: declare -x VE env
+*/
+int	export_out_main(t_main *mini)
+{
+	char	*bfore;
+	char	*after;
+	char	**env_sort;
+	int		i;
+
+	bfore = NULL;
+	after = NULL;
+	env_sort = ft_sort(&mini->env_list);
+	i = 0;
+	while (env_sort[i])
+	{
+		bfore = ft_strdupto_n(env_sort[i], '=');
+		after = ft_strdup_from(env_sort[i], '=');
+		if (ft_strncmp(bfore, "?=", 2) == 0)
+			i ++;
+		else
+			ft_printf("declare -x %s=\"%s\"\n", bfore, after);
+		i ++;
+	}
+	mini->shell.error = handle_error_bis(0);
+	return (0);
+}
+
+/* 
+ * Mini parsing des args d'export
+ * Check si le nom est conforme, juste des chiffres, des lettres et des
+ * underscores
+*/
+int	parse_export(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!(str[0] >= 'A' && str[0] <= 'Z') && !(str[0] >= 'a'
+			&& str[0] <= 'z'))
+		return (1);
+	while (str[i] && str[i] != '=')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9') && !(str[i] \
+			>= 'A' && str[i] <= 'Z') && !(str[i] >= 'a' \
+			&& str[i] <= 'z') && str[i] != '_')
+			return (1);
+		i ++;
+	}
+	if (str[i] == '=')
+		return (0);
+	return (2);
+}
